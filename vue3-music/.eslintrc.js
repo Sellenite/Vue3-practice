8819fc7078696f2250f757b7ae7a6a97a3703004
Sellenite <714627528@qq.com>
2021-04-14 16:21:16 +0800

    slider

--- vue3-music/.eslintrc.js
+++ vue3-music/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'indent': 0,
     'space-before-function-paren': 0,
-    'semi': 0
+    'semi': 0,
+    'eol-last': 0
   }
 }
